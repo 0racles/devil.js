a243f196f082925e74132469d3b588818f44c586
0racles <oracleslonge@gmail.com>
2018-02-01 16:52:29 +0000

    added main_app.js

---
+++ src/main_app.js
@@ -0,0 +1,14 @@
+var css = require('../src/assets/sass/global.scss'), 
+		images = require('../src/assets/sass/components/image.scss'),
+		JqueryMatchHeight = require('jquery-match-height'),
+		// enable this to use font awesome's default settings
+		fontAwesome = require('font-awesome-sass-loader');
+		//  enable this to use custom settings for app per client
+		
+
+$(document).ready(function () {
+
+
+});
+
+// this script should be well commented
